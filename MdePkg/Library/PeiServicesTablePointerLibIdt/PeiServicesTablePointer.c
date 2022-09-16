0572a79259e5e584bbd6d5c04e558576814d1911
Ray Ni <ray.ni@intel.com>
2022-09-16 16:17:15 +0800

    Revert "Save PeiService Pointer in FSBASE"
    This reverts commit 9e9fb507fd21d1f75d93f975c328dcdf0758f09a.

--- MdePkg/Library/PeiServicesTablePointerLibIdt/PeiServicesTablePointer.c
+++ MdePkg/Library/PeiServicesTablePointerLibIdt/PeiServicesTablePointer.c
@@ -15,17 +15,6 @@
 #include <Library/PeiServicesTablePointerLib.h>
 #include <Library/DebugLib.h>
 #include <Library/BaseMemoryLib.h>
-VOID
-EFIAPI
-AsmWriteFsBase (
-  UINT64  FsBase
-  );
-
-UINT64
-EFIAPI
-AsmReadFsBase (
-  VOID
-  );
 
 /**
   Retrieves the cached value of the PEI Services Table pointer.
@@ -46,8 +35,10 @@ GetPeiServicesTablePointer (
   )
 {
   CONST EFI_PEI_SERVICES  **PeiServices;
+  IA32_DESCRIPTOR         Idtr;
 
-  PeiServices = (CONST EFI_PEI_SERVICES **)(UINTN)AsmReadFsBase ();
+  AsmReadIdtr (&Idtr);
+  PeiServices = (CONST EFI_PEI_SERVICES **)(*(UINTN *)(Idtr.Base - sizeof (UINTN)));
   ASSERT (PeiServices != NULL);
   return PeiServices;
 }
@@ -71,8 +62,11 @@ SetPeiServicesTablePointer (
   IN CONST EFI_PEI_SERVICES  **PeiServicesTablePointer
   )
 {
+  IA32_DESCRIPTOR  Idtr;
+
   ASSERT (PeiServicesTablePointer != NULL);
-  AsmWriteFsBase ((UINTN)PeiServicesTablePointer);
+  AsmReadIdtr (&Idtr);
+  (*(UINTN *)(Idtr.Base - sizeof (UINTN))) = (UINTN)PeiServicesTablePointer;
 }
 
 /**
@@ -97,7 +91,6 @@ MigratePeiServicesTablePointer (
   VOID
   )
 {
-  /*
   EFI_STATUS              Status;
   IA32_DESCRIPTOR         Idtr;
   EFI_PHYSICAL_ADDRESS    IdtBase;
@@ -125,6 +118,6 @@ MigratePeiServicesTablePointer (
   CopyMem ((VOID *)(UINTN)IdtBase, (VOID *)(Idtr.Base - sizeof (UINTN)), Idtr.Limit + 1 + sizeof (UINTN));
   Idtr.Base = (UINTN)IdtBase + sizeof (UINTN);
   AsmWriteIdtr (&Idtr);
-  */
+
   return;
 }
