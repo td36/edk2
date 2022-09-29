db4ec9c224082fd0753c451d84aa0c977aec1a43
Ray Ni <ray.ni@intel.com>
2022-09-29 12:23:55 +0800

    UefiCpuPkg/MtrrLib: Fix MtrrGetAllMtrrs to return correct MTRR setting
    Old implementation of MtrrGetAllMtrrs has following issues:
    1. MtrrSetting contains random value if MTRR is not supported.
    2. Unconditionally access fixed MTRR on CPU that may not support
        fixed MTRR.
    3. Variable MTRR entries starting from 10 remain uninitialized if
        max number of variable MTRRs is 10.
    
    The patch fixes all of above issues.
    
    Signed-off-by: Ray Ni <ray.ni@intel.com>
    Cc: Eric Dong <eric.dong@intel.com>
    Cc: Rahul Kumar <rahul1.kumar@intel.com>

--- UefiCpuPkg/Library/MtrrLib/MtrrLib.c
+++ UefiCpuPkg/Library/MtrrLib/MtrrLib.c
@@ -2819,28 +2819,38 @@ MtrrGetAllMtrrs (
   OUT MTRR_SETTINGS  *MtrrSetting
   )
 {
-  if (!IsMtrrSupported ()) {
+  BOOLEAN                          FixedMtrrSupported;
+  UINT32                           VariableMtrrCount;
+  MSR_IA32_MTRR_DEF_TYPE_REGISTER  *MtrrDefType;
+
+  ZeroMem (MtrrSetting, sizeof (*MtrrSetting));
+
+  MtrrDefType = (MSR_IA32_MTRR_DEF_TYPE_REGISTER *)&MtrrSetting->MtrrDefType;
+  if (!MtrrLibIsMtrrSupported (&FixedMtrrSupported, &VariableMtrrCount)) {
     return MtrrSetting;
   }
 
   //
+  // Get MTRR_DEF_TYPE value
+  //
+  MtrrDefType->Uint64 = AsmReadMsr64 (MSR_IA32_MTRR_DEF_TYPE);
+  ASSERT (FixedMtrrSupported || (MtrrDefType->Bits.FE == 0));
+
+  //
   // Get fixed MTRRs
   //
-  MtrrGetFixedMtrrWorker (&MtrrSetting->Fixed);
+  if (MtrrDefType->Bits.FE == 1) {
+    MtrrGetFixedMtrrWorker (&MtrrSetting->Fixed);
+  }
 
   //
   // Get variable MTRRs
   //
   MtrrGetVariableMtrrWorker (
     NULL,
-    GetVariableMtrrCountWorker (),
+    VariableMtrrCount,
     &MtrrSetting->Variables
     );
-
-  //
-  // Get MTRR_DEF_TYPE value
-  //
-  MtrrSetting->MtrrDefType = AsmReadMsr64 (MSR_IA32_MTRR_DEF_TYPE);
 
   return MtrrSetting;
 }
@@ -2902,10 +2912,12 @@ IsMtrrSupported (
 {
   BOOLEAN  FixedMtrrSupported;
   UINT32   VariableMtrrCount;
+
   MtrrLibIsMtrrSupported (&FixedMtrrSupported, &VariableMtrrCount);
   if (!FixedMtrrSupported || (VariableMtrrCount == 0)) {
     return FALSE;
   }
+
   return TRUE;
 }
 
