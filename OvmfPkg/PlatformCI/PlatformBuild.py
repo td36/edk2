97f1912820bcb3c01f8bdc6c3a1795aad6c7fdda
Dun Tan <dun.tan@intel.com>
2022-11-16 01:05:36 +0800

    V2
    Signed-off-by: Dun Tan <dun.tan@intel.com>
    Signed-off-by: Dun Tan <dun.tan@intel.com>

--- OvmfPkg/PlatformCI/PlatformBuild.py
+++ OvmfPkg/PlatformCI/PlatformBuild.py
@@ -4,8 +4,13 @@
 # Copyright (c) Microsoft Corporation.
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 ##
+from asyncio.windows_events import NULL
+import shutil
 import os
 import sys
+import logging
+from edk2toollib.utility_functions import RunCmd
+from edk2toolext.environment import shell_environment
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from PlatformBuildLib import SettingsManager
@@ -17,6 +22,8 @@ from PlatformBuildLib import PlatformBuilder
 class CommonPlatform():
     ''' Common settings for this platform.  Define static data here and use
         for the different parts of stuart
+
+        PackagesSupported = ("OvmfPkg", "UefiCpuPkg", "MdeModulePkg")
     '''
     PackagesSupported = ("OvmfPkg",)
     ArchSupported = ("IA32", "X64")
@@ -38,6 +45,158 @@ class CommonPlatform():
             dsc += "X64"
         dsc += ".dsc"
         return dsc
+        #return "TargetUnitTest/OvmfPkgTargetTest.dsc"
+
+class UnitTestSupport():
+    ''' Common settings for this platform.  Define static data here and use
+        for the different parts of stuart
+    
+    UnitTestModule    = {'UefiCpuPkg/Library/CpuExceptionHandlerLib/UnitTest/DxeCpuExceptionHandlerLibUnitTest.inf':'UefiCpuPkg.dsc',
+                         'MdeModulePkg/Application/HelloWorld/HelloWorld.inf'                                      :'MdeModulePkg.dsc'
+                        }
+    UTPkgsToBuild   = ["UefiCpuPkg", "MdeModulePkg"]
+    '''
+    UnitTestModule = {}
+    UTPkgsToBuild  = []
+
+    def UpdateUnitTestConfig(self, args):
+        '''Update UnitTestModule, PackagesSupported and UTPkgsToBuild in UnitTestSupport class'''
+        self.env = shell_environment.GetBuildVars()
+        UnitTestModuleList = ';'.join(args.ShellUnitTestList).split(";")
+        if not UnitTestModuleList:
+            return
+        PackagesSupported = list(CommonPlatform.PackagesSupported)
+        for Module in UnitTestModuleList:
+            PkgName = Module.split("Pkg")[0] + 'Pkg'
+            if PkgName not in UnitTestSupport.UnitTestModule.keys():
+                UnitTestSupport.UnitTestModule[Module] = PkgName + '.dsc'
+            if PkgName not in PackagesSupported:
+                PackagesSupported.append(PkgName)
+        CommonPlatform.PackagesSupported = tuple(PackagesSupported)
+        print('PackagesSupported is {}'.format(CommonPlatform.PackagesSupported))
+        print('UnitTestModule    is {}'.format(UnitTestSupport.UnitTestModule))
+        try:
+            UTPkgsToBuild = ','.join(args.PackagesForUT).split(',')
+            if UTPkgsToBuild:
+                UnitTestSupport.UTPkgsToBuild = UTPkgsToBuild
+                print('UTPkgsToBuild is {}'.format(UnitTestSupport.UTPkgsToBuild))
+        except:
+            pass
+
+    def BuildUnitTest(self):
+        UTPkgsToBuild = UnitTestSupport.UTPkgsToBuild
+        if not UTPkgsToBuild:
+            return
+        self.VirtualDrive = os.path.join(self.env.GetValue("BUILD_OUTPUT_BASE"), "VirtualDrive")
+        # OvmfPkg should have been built in BUILD process.
+        BuiltDsc = ['OvmfPkg']
+        for package in UTPkgsToBuild:
+            print(package)
+            for Module, Dsc in UnitTestSupport.UnitTestModule.items():
+                if package in Dsc:
+                    if Dsc not in BuiltDsc:
+                        ModuleName = os.path.normpath(Module).split('.inf')[0].rsplit('\\')[-1]
+                        BuiltDsc.append(Dsc)
+                        DscPath = os.path.join(package, Dsc)
+                        # Build specific dsc for UnitTest modules
+                        print('Going to build this {0} for {1}'.format(DscPath, ModuleName))
+                        Arch = self.env.GetValue("TARGET_ARCH").split(" ")
+                        # Set the Unit Test arch the same as the Shell in Ovmf.
+                        if 'X64' in Arch:
+                            UTArch = 'X64'
+                        else:
+                            UTArch = 'IA32'
+                        self.env.AllowOverride("ACTIVE_PLATFORM")
+                        self.env.SetValue("ACTIVE_PLATFORM", DscPath, "Test")
+                        self.env.AllowOverride("TARGET_ARCH")
+                        self.env.SetValue("TARGET_ARCH", UTArch, "Test")
+                        PlatformBuilder.Build(self)
+                    # Copy the UnitTest efi files to VirtualDrive folder
+                    EfiPath = os.path.join(self.env.GetValue("BUILD_OUTPUT_BASE"), UTArch, Module.split('.inf')[0], self.env.GetValue("TARGET"), ModuleName + '.efi')
+                    print('Copy {0}.efi from:{1}'.format(ModuleName, EfiPath))
+                    shutil.copy(EfiPath, self.VirtualDrive)
+
+    def WriteEfiToStartup(self):
+        ''' Write all the .efi files' name in VirtualDrive into Startup.nsh'''
+        if (self.env.GetValue("MAKE_STARTUP_NSH").upper() == "TRUE"):
+            f = open(os.path.join(self.VirtualDrive, "startup.nsh"), "w")
+            for root,dirs,files in os.walk(self.VirtualDrive):
+                for file in files:
+                    print(file)
+                    print(os.path.splitext(file)[1])
+                    if os.path.splitext(file)[1] == '.efi':
+                        f.write("{0} \n".format(file))
+            f.write("reset -s\n")
+            f.close()
+
+    def CheckBootLog(self):
+        #
+        # Find all FAILURE MESSAGE in boot log
+        #
+        if not UnitTestSupport.UTPkgsToBuild:
+            return 0
+        #BuildLog = "BUILDLOG_{0}.txt".format(PlatformBuilder.GetName(self))
+        #LogPath  = os.path.join(self.ws, 'Build', BuildLog)
+        LogPath = 'E:/code/FutureCoreRpArraw/RomImages/fail.log'
+        print('Checking the boot log: {0}'.format(LogPath))
+        file        = open(LogPath, "r")
+        fileContent = file.readlines()
+        for Index in range(len(fileContent)):
+            if 'FAILURE MESSAGE:' in fileContent[Index]:
+                if fileContent[Index + 1].strip() != '':
+                    FailureMessage = fileContent[Index + 1] + fileContent[Index + 2]
+                    print(FailureMessage)
+                    return FailureMessage
+        return 0
 
 import PlatformBuildLib
+UnitTestUnitTestConfig = UnitTestSupport()
 PlatformBuildLib.CommonPlatform = CommonPlatform
+
+class UnitTestSettingsManager(SettingsManager):
+    def GetPlatformDscAndConfig(self) -> tuple:
+        return None
+    def AddCommandLineOptions(self, parserObj):
+        print('AddCommandLineOptions in PrSettingsManager')
+        parserObj.add_argument('ShellUnitTestList', type=str,
+                               help='Optional - A package or folder you want to update (workspace relative).'
+                               'Can list multiple by doing -p <pkg1>,<pkg2> or -p <pkg3> -p <pkg4>',
+                               action="append", default=[])
+
+    def RetrieveCommandLineOptions(self, args):
+        print('RetrieveCommandLineOptions in PrSettingsManager')
+        UnitTestUnitTestConfig.UpdateUnitTestConfig(args)
+
+class UnitTestPlatformBuilder(PlatformBuilder):
+    def AddCommandLineOptions(self, parserObj):
+        ''' Add command line options to the argparser '''
+        print('AddCommandLineOptions in PlatformBuilder')
+        PlatformBuilder.AddCommandLineOptions(self, parserObj)
+        parserObj.add_argument('-p', '--pkg', '--pkg-dir', dest='PackagesForUT', type=str,
+                               help='Optional - A package or folder you want to update (workspace relative).'
+                               'Can list multiple by doing -p <pkg1>,<pkg2> or -p <pkg3> -p <pkg4>',
+                               action="append", default=[])
+        parserObj.add_argument('ShellUnitTestList', type=str,
+                               help='Optional - A package or folder you want to update (workspace relative).'
+                               'Can list multiple by doing -p <pkg1>,<pkg2> or -p <pkg3> -p <pkg4>',
+                               action="append", default=['None'])
+
+    def RetrieveCommandLineOptions(self, args):
+        '''  Retrieve command line options from the argparser '''
+        print('RetrieveCommandLineOptions in PlatformBuilder')
+        PlatformBuilder.RetrieveCommandLineOptions(self, args)
+        UnitTestUnitTestConfig.UpdateUnitTestConfig(args)
+
+    def PlatformPostBuild(self):
+        ''' Build specific Pkg in command line for UnitTest modules.'''
+        UnitTestUnitTestConfig.BuildUnitTest()
+        UnitTestUnitTestConfig.WriteEfiToStartup()
+        return 0
+
+    def FlashRomImage(self):
+        PlatformBuilder.FlashRomImage(self)
+        UnitTestResult = UnitTestUnitTestConfig.CheckBootLog()
+        if (UnitTestResult):
+            logging.info("UnitTest failed with this FAILURE MESSAGE:\n{}".format(UnitTestResult))
+            return UnitTestResult
+        return 0
--- OvmfPkg/PlatformCI/TargetTestPlatformBuild.py
+++ OvmfPkg/PlatformCI/TargetTestPlatformBuild.py
@@ -20,96 +20,95 @@ from PlatformBuildLib import PlatformBuilder
     # ####################################################################################### #
     #                                Common Configuration                                     #
     # ####################################################################################### #
-class UnitTestConfig():
+class CommonPlatform():
     ''' Common settings for this platform.  Define static data here and use
         for the different parts of stuart
+
+        PackagesSupported = ("OvmfPkg", "UefiCpuPkg", "MdeModulePkg")
     '''
+    PackagesSupported = ("OvmfPkg",)
+    ArchSupported = ("IA32", "X64")
+    TargetsSupported = ("DEBUG", "RELEASE", "NOOPT")
+    Scopes = ('ovmf', 'edk2-build')
+    WorkspaceRoot = os.path.realpath(os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "..", ".."))
+
+    @classmethod
+    def GetDscName(cls, ArchCsv: str) -> str:
+        ''' return the DSC given the architectures requested.
+
+        ArchCsv: csv string containing all architectures to build
+        
+        dsc = "OvmfPkg"
+        if "IA32" in ArchCsv.upper().split(","):
+            dsc += "Ia32"
+        if "X64" in ArchCsv.upper().split(","):
+            dsc += "X64"
+        dsc += ".dsc"
+        return dsc
+        '''
+        return "TargetUnitTest/OvmfPkgTargetTest.dsc"
+
+class UnitTestSupport():
+    ''' Common settings for this platform.  Define static data here and use
+        for the different parts of stuart
+    
     UnitTestModule    = {'UefiCpuPkg/Library/CpuExceptionHandlerLib/UnitTest/DxeCpuExceptionHandlerLibUnitTest.inf':'UefiCpuPkg.dsc',
                          'MdeModulePkg/Application/HelloWorld/HelloWorld.inf'                                      :'MdeModulePkg.dsc'
                         }
-    PackagesSupported = ("UefiCpuPkg",)
-    ArchSupported     = ("IA32", "X64")
-    TargetsSupported  = ("DEBUG", "RELEASE", "NOOPT")
-    Scopes            = ('ovmf', 'edk2-build')
-    WorkspaceRoot     = os.path.realpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".."))
-    def __init__(self):
-        print('Print2')
-        for Value in UnitTestConfig.UnitTestModule.values():
-            PkgName = Value.split("Pkg")[0] + 'Pkg'
-            PackagesSupported = list(UnitTestConfig.PackagesSupported)
-            UnitTestConfig.PackagesSupported = PackagesSupported
-            if PkgName not in list(UnitTestConfig.PackagesSupported):
-                UnitTestConfig.PackagesSupported.append(PkgName)
+    PackagesToBuild   = ["UefiCpuPkg", "MdeModulePkg"]
+    '''
+    UnitTestModule  = {}
+    PackagesToBuild = []
 
-    def GetDscName(cls, ArchCsv: str) -> str:
-        return "TargetUnitTest/OvmfPkgTargetTest.dsc"
-print('Print1')
-import PlatformBuildLib
-UnitTestUnitTestConfig = UnitTestConfig()
-PlatformBuildLib.CommonPlatform = UnitTestUnitTestConfig
-
-class UnitTestSettingsManager(SettingsManager):
-    def GetPlatformDscAndConfig(self) -> tuple:
-        return None
-
-class UnitTestPlatformBuilder(PlatformBuilder):
-    def AddCommandLineOptions(self, parserObj):
-        print('Print3')
-        ''' Add command line options to the argparser '''
-        parserObj.add_argument('-a', "--arch", dest="build_arch", type=str, default="IA32,X64",
-            help="Optional - CSV of architecture to build.  IA32 will use IA32 for Pei & Dxe. "
-            "X64 will use X64 for both PEI and DXE.  IA32,X64 will use IA32 for PEI and "
-            "X64 for DXE. default is IA32,X64")
-        parserObj.add_argument('-p', '--pkg', '--pkg-dir', dest='packageList', type=str,
-                               help='Optional - A package or folder you want to update (workspace relative).'
-                               'Can list multiple by doing -p <pkg1>,<pkg2> or -p <pkg3> -p <pkg4>',
-                               action="append", default=[])
-        
-    def CheckBootLog(self):
-        #
-        # Find all FAILURE MESSAGE in boot log
-        #
-        #BuildLog = "BUILDLOG_{0}.txt".format(PlatformBuilder.GetName(self))
-        #LogPath  = os.path.join(self.ws, 'Build', BuildLog)
-        LogPath = 'E:/code/FutureCoreRpArraw/RomImages/fail.log'
-        print('Checking the boot log: {0}'.format(LogPath))
-        file        = open(LogPath, "r")
-        fileContent = file.readlines()
-        for Index in range(len(fileContent)):
-            if 'FAILURE MESSAGE:' in fileContent[Index]:
-                if fileContent[Index + 1].strip() != '':
-                    FailureMessage = fileContent[Index + 1] + fileContent[Index + 2]
-                    print(FailureMessage)
-                    return FailureMessage
-        return 0
+    def UpdateUnitTestConfig(self, args):
+        self.env = shell_environment.GetBuildVars()
+        '''Update UnitTestModule, PackagesSupported and PackagesToBuild in UnitTestSupport class'''
+        UnitTestModuleList = ';'.join(args.ShellUnitTestList).split(";")
+        if not UnitTestModuleList:
+            return
+        PackagesSupported = list(CommonPlatform.PackagesSupported)
+        for Module in UnitTestModuleList:
+            PkgName = Module.split("Pkg")[0] + 'Pkg'
+            if PkgName not in UnitTestSupport.UnitTestModule.keys():
+                UnitTestSupport.UnitTestModule[Module] = PkgName + '.dsc'
+            if PkgName not in PackagesSupported:
+                PackagesSupported.append(PkgName)
+        CommonPlatform.PackagesSupported = tuple(PackagesSupported)
+        print('PackagesSupported is {}'.format(CommonPlatform.PackagesSupported))
+        print('UnitTestModule    is {}'.format(UnitTestSupport.UnitTestModule))
+        try:
+            PackagesToBuild = ','.join(args.PackagesForUT).split(',')
+            if PackagesToBuild:
+                UnitTestSupport.PackagesToBuild = PackagesToBuild
+                print('PackagesToBuild is {}'.format(UnitTestSupport.PackagesToBuild))
+        except:
+            pass
 
     def BuildUnitTest(self, packageList):
         VirtualDrive = os.path.join(self.env.GetValue("BUILD_OUTPUT_BASE"), "VirtualDrive")
         BuiltDsc = []
         for package in packageList:
             print(package)
-            for Module, Dsc in UnitTestConfig.UnitTestModule.items():
-                if package in Dsc and Dsc not in BuiltDsc:
-                    Module = os.path.normpath(Module)
-                    ModuleName = Module.split('.inf')[0].rsplit('\\')[-1]
-                    BuiltDsc.append(Dsc)
-                    DscPath = os.path.join(package, Dsc)
-
-                    # Build specific dsc for UnitTest modules
-                    print('Going to build this {0} for {1}'.format(DscPath, ModuleName))
-                    Arch = self.env.GetValue("TARGET_ARCH").split(" ")
-                    print('Arch is {}'.format(Arch))
-                    # Set the Unit Test arch the same as the Shell in Ovmf.
-                    if 'X64' in Arch:
-                        UTArch = 'X64'
-                    else:
-                        UTArch = 'IA32'
-                    self.env.AllowOverride("ACTIVE_PLATFORM")
-                    self.env.SetValue("ACTIVE_PLATFORM", DscPath, "Test")
-                    self.env.AllowOverride("TARGET_ARCH")
-                    self.env.SetValue("TARGET_ARCH", UTArch, "Test")
-                    self.Build()
-
+            for Module, Dsc in UnitTestSupport.UnitTestModule.items():
+                if package in Dsc:
+                    if Dsc not in BuiltDsc:
+                        ModuleName = os.path.normpath(Module).split('.inf')[0].rsplit('\\')[-1]
+                        BuiltDsc.append(Dsc)
+                        DscPath = os.path.join(package, Dsc)
+                        # Build specific dsc for UnitTest modules
+                        print('Going to build this {0} for {1}'.format(DscPath, ModuleName))
+                        Arch = self.env.GetValue("TARGET_ARCH").split(" ")
+                        # Set the Unit Test arch the same as the Shell in Ovmf.
+                        if 'X64' in Arch:
+                            UTArch = 'X64'
+                        else:
+                            UTArch = 'IA32'
+                        self.env.AllowOverride("ACTIVE_PLATFORM")
+                        self.env.SetValue("ACTIVE_PLATFORM", DscPath, "Test")
+                        self.env.AllowOverride("TARGET_ARCH")
+                        self.env.SetValue("TARGET_ARCH", UTArch, "Test")
+                        PlatformBuilder.Build(self)
                     # Copy the UnitTest efi files to VirtualDrive folder
                     EfiPath = os.path.join(self.env.GetValue("BUILD_OUTPUT_BASE"), UTArch, Module.split('.inf')[0], self.env.GetValue("TARGET"), ModuleName + '.efi')
                     print('Copy {0}.efi from:{1}'.format(ModuleName, EfiPath))
@@ -128,32 +127,72 @@ class UnitTestPlatformBuilder(PlatformBuilder):
             f.write("reset -s\n")
             f.close()
 
-    def PlatformPostBuild(self):
-        ''' Build specific Pkg in command line for UnitTest modules.
-            The build configuration is defined in Dsc of UnitTestConfig.GetDscName.
-            Then copy the .efi files into VirtualDrive folder.
-        '''
-        parserObj = argparse.ArgumentParser(description='For building UnitTest modules')
-        self.AddCommandLineOptions(parserObj)
-        args, unknown_args = parserObj.parse_known_args()
-        print(args.packageList)
-        packageListSet = set()
-        for item in args.packageList:  # Parse out the individual packages
-            item_list = item.split(",")
-            print(item_list)
-            for individual_item in item_list:
-                # in case cmd line caller used Windows folder slashes
-                individual_item = individual_item.replace("\\", "/").rstrip("/")
-                packageListSet.add(individual_item.strip())
+    def CheckBootLog(self):
+        #
+        # Find all FAILURE MESSAGE in boot log
+        #
+        #BuildLog = "BUILDLOG_{0}.txt".format(PlatformBuilder.GetName(self))
+        #LogPath  = os.path.join(self.ws, 'Build', BuildLog)
+        LogPath = 'E:/code/FutureCoreRpArraw/RomImages/fail.log'
+        print('Checking the boot log: {0}'.format(LogPath))
+        file        = open(LogPath, "r")
+        fileContent = file.readlines()
+        for Index in range(len(fileContent)):
+            if 'FAILURE MESSAGE:' in fileContent[Index]:
+                if fileContent[Index + 1].strip() != '':
+                    FailureMessage = fileContent[Index + 1] + fileContent[Index + 2]
+                    print(FailureMessage)
+                    return FailureMessage
+        return 0
 
-        self.BuildUnitTest(packageListSet)
+import PlatformBuildLib
+UnitTestUnitTestConfig = UnitTestSupport()
+PlatformBuildLib.CommonPlatform = CommonPlatform
+
+class UnitTestSettingsManager(SettingsManager):
+    def GetPlatformDscAndConfig(self) -> tuple:
+        return None
+    def AddCommandLineOptions(self, parserObj):
+        print('AddCommandLineOptions in PrSettingsManager')
+        parserObj.add_argument('ShellUnitTestList', type=str,
+                               help='Optional - A package or folder you want to update (workspace relative).'
+                               'Can list multiple by doing -p <pkg1>,<pkg2> or -p <pkg3> -p <pkg4>',
+                               action="append", default=[])
+
+    def RetrieveCommandLineOptions(self, args):
+        print('RetrieveCommandLineOptions in PrSettingsManager')
+        UnitTestUnitTestConfig.UpdateUnitTestConfig(args)
+
+class UnitTestPlatformBuilder(PlatformBuilder):
+    def AddCommandLineOptions(self, parserObj):
+        ''' Add command line options to the argparser '''
+        print('AddCommandLineOptions in PlatformBuilder')
+        PlatformBuilder.AddCommandLineOptions(self, parserObj)
+        parserObj.add_argument('-p', '--pkg', '--pkg-dir', dest='PackagesForUT', type=str,
+                               help='Optional - A package or folder you want to update (workspace relative).'
+                               'Can list multiple by doing -p <pkg1>,<pkg2> or -p <pkg3> -p <pkg4>',
+                               action="append", default=[])
+        parserObj.add_argument('ShellUnitTestList', type=str,
+                               help='Optional - A package or folder you want to update (workspace relative).'
+                               'Can list multiple by doing -p <pkg1>,<pkg2> or -p <pkg3> -p <pkg4>',
+                               action="append", default=['None'])
+
+    def RetrieveCommandLineOptions(self, args):
+        '''  Retrieve command line options from the argparser '''
+        print('RetrieveCommandLineOptions in PlatformBuilder')
+        PlatformBuilder.RetrieveCommandLineOptions(self, args)
+        UnitTestUnitTestConfig.UpdateUnitTestConfig(args)
+
+    def PlatformPostBuild(self):
+        ''' Build specific Pkg in command line for UnitTest modules.'''
+        UnitTestUnitTestConfig.BuildUnitTest(UnitTestSupport.PackagesToBuild)
         VirtualDrive = os.path.join(self.env.GetValue("BUILD_OUTPUT_BASE"), "VirtualDrive")
-        self.WriteEfiToStartup(VirtualDrive)
+        UnitTestUnitTestConfig.WriteEfiToStartup(VirtualDrive)
         return 0
 
     def FlashRomImage(self):
         PlatformBuilder.FlashRomImage(self)
-        UnitTestResult = self.CheckBootLog()
+        UnitTestResult = UnitTestUnitTestConfig.CheckBootLog()
         if (UnitTestResult):
             logging.info("UnitTest failed with this FAILURE MESSAGE:\n{}".format(UnitTestResult))
             return UnitTestResult
